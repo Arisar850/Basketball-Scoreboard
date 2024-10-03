2a784e0daa388c6be37d324c89d624d8ee82e165
Abbas Ali <128288820+Arisar850@users.noreply.github.com>
2024-10-03 13:03:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let count_home = 0
+let count_guest = 0
+
+function add1_home(){
+    count_home+=1
+    document.getElementById('count-home-result').innerText = count_home
+
+}
+function add2_home(){
+    count_home+=2
+    document.getElementById('count-home-result').innerText = count_home
+
+}
+function add3_home(){
+    count_home+=3
+    document.getElementById('count-home-result').innerText = count_home
+
+}
+function add1_guest(){
+    count_guest+=1
+    document.getElementById('count-guest-result').innerText = count_guest
+
+}
+function add2_guest(){
+    count_guest+=2
+    document.getElementById('count-guest-result').innerText = count_guest
+
+}
+function add3_guest(){
+    count_guest+=3
+    document.getElementById('count-guest-result').innerText = count_guest
+
+}
